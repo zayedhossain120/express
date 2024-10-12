fcfd8a49fe37d9a4b11da4ecafc324331d263deb
zayedhossain120 <zayedhossain120@gmail/com>
2024-10-12 13:16:15 +0600

    working on CRUD on post

---
+++ example.js
@@ -0,0 +1,18 @@
+// here all code are tested and working
+
+// how to run a static route ===== start ===
+
+// ===== proper form ====
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "index.html"));
+// });
+// app.get("/about", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "about.html"));
+// });
+
+// ===== short form ====
+
+// app.use(express.static(path.join(__dirname, "public")));
+
+// how to run a static route ===== end ===
--- server.js
+++ server.js
@@ -1,11 +1,48 @@
 const express = require("express");
+const path = require("path");
+
+const port = process.env.PORT || 8000;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello Bangladesh");
+const posts = [
+  {
+    id: 1,
+    title: "Post one",
+  },
+  {
+    id: 2,
+    title: "Post two",
+  },
+  {
+    id: 3,
+    title: "Post three",
+  },
+  {
+    id: 4,
+    title: "Post Four",
+  },
+  {
+    id: 5,
+    title: "Post five",
+  },
+];
+
+app.get("/api/posts", (req, res) => {
+  const limit = parseInt(req.query.limit);
+  console.log(isNaN(limit));
+
+  if (!isNaN(limit)) {
+  }
+
+  res.json(posts);
 });
 
-app.listen(8000, () => {
-  console.log(`Server is runnig on port 8000`);
+app.get("/api/posts/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  res.json(posts.find((post) => post.id == id));
+});
+
+app.listen(port, () => {
+  console.log(`Server is runnig on port ${port}`);
 });
